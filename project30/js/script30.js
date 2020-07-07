25a14a35d5925d4f3b4db2ef015aa82c558b22a9
vladiko <ozonhold@inbox.ru>
2020-07-07 21:51:29 +0300

    practic lesson 33

--- project30/js/script30.js
+++ project30/js/script30.js
@@ -1,50 +1,115 @@
-/* Задания на урок:
+/* Задания на урок#33 :
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),//реклама справа картинки
+        bg = document.querySelector('.promo__bg'),//блок текста в центре
+        genre = bg.querySelector('.promo__genre'),//строка комедия в блоке текста
+        list = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),//родитель списка фильмов
+       checkbox = addForm.querySelector('[type = "checkbox"]'); 
+        // btn = document.getElementsByTagName('button'),//"кнопка подтвердить"
+      
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-const adv = document.querySelectorAll('.promo__adv img'),//реклама справа картинки
-    bg = document.querySelector('.promo__bg'),//блок текста в центре
-    genre = bg.querySelector('.promo__genre'),//строка комедия в блоке текста
-    list = document.querySelector('.promo__interactive-list');//родитель списка фильмов
+        let newFilm = addInput.value;
+        const favorit = checkbox.checked;
 
-adv.forEach(item => {
-    item.remove();
-});//задание 1
+        if (newFilm) {
 
-genre.textContent = 'драма';//задание 2
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            if (favorit) {
+                console.log("Добавляем любимый фильм");
+            }
 
-bg.style.backgroundImage = 'url("img/bg.jpg")';//задание 3
+        movieDB.movies.push(newFilm);
+        sortArr(movieDB.movies);
 
-list.innerHTML = "";//очистка списка фильмов
+        createMoviList(movieDB.movies, list);     
+        }
+        
+        event.target.reset();
 
-movieDB.movies.sort();//сортировка объекта с фильмами
+    });
 
-movieDB.movies.forEach((film, i) => {
-    list.innerHTML +=`
-    <li class="promo__interactive-item">${i + 1} ${film}
-        <div class="delete"></div>
-    </li>
-    `;//формирование замещающей динамической html структуры с нумерацией и фильмами  
-});//задания 4 и 5
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });//задание 1
+    };
+
+    
+
+    const makeChanges = () => {
+    genre.textContent = 'драма';//задание 2
+    
+    bg.style.backgroundImage = 'url("img/bg.jpg")';//задание 3
+    };
+    
+    
+    
+    const sortArr = (arr) => {
+      arr.sort();//сортировка объекта с фильмами  
+    };
+
+    
+
+    function createMoviList(films, parent) {
+    parent.innerHTML = "";//очистка списка фильмов
+
+    sortArr(films);
+    
+    films.forEach((film, i) => {
+        parent.innerHTML +=`
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>`;//формирование замещающей динамической html структуры с нумерацией и фильмами  
+    });//задания 4 и 5
+
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+
+            createMoviList(films, parent);
+        });
+    });
+
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMoviList(movieDB.movies, list);
+
+});
